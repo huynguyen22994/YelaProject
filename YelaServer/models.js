59051ff1ebf27f417a24db5cf1a681d11a1804b6
Vu Nguyen <vundm@imt-soft.com>
2017-11-11 11:07:35 +0700

    - update stype for no result box

--- YelaServer/models.js
+++ YelaServer/models.js
@@ -1,72 +1,73 @@
-var Sequelize = require('sequelize');
-var fs = require('fs');
-var data = fs.readFileSync('./config/config.json');
+// var Sequelize = require('sequelize');
+// var fs = require('fs');
+// var data = fs.readFileSync('./config/config.json');
+var models = require('./app/models');
+models.sequelize.sync();
+// var db = {};
 
-var db = {};
+// // parse Json to Object
+// var databaseConfig = JSON.parse(data.toString());
+// var database = databaseConfig.database;
 
-// parse Json to Object
-var databaseConfig = JSON.parse(data.toString());
-var database = databaseConfig.database;
+// //sequelize
+// var sequelize = new Sequelize(database.dbname, database.username, database.password, {
+//   host: database.host,
+//   dialect: database.dialect,
 
-//sequelize
-var sequelize = new Sequelize(database.dbname, database.username, database.password, {
-  host: database.host,
-  dialect: database.dialect,
+//   pool: {
+//     max: database.max,
+//     min: database.min,
+//     idle: database.idles
+//   },
 
-  pool: {
-    max: database.max,
-    min: database.min,
-    idle: database.idles
-  },
+//   // SQLite only
+//   //storage: 'http://localhost/phpmyadmin/to/database.sqlite'
+// });
 
-  // SQLite only
-  //storage: 'http://localhost/phpmyadmin/to/database.sqlite'
-});
-
-var Category = sequelize.define('Category', {
-    categoryId: {
-        type: Sequelize.UUID,
-        defaultValue: Sequelize.UUIDV1,
-        allowNull: false,
-        primaryKey: true
-    },
-    name: {
-        type: Sequelize.STRING,
-        allowNull: false
-    }
-}, {
-    charset: 'utf8',
-    collate: 'utf8_unicode_ci'
-    });
+// var Category = sequelize.define('Category', {
+//     categoryId: {
+//         type: Sequelize.UUID,
+//         defaultValue: Sequelize.UUIDV1,
+//         allowNull: false,
+//         primaryKey: true
+//     },
+//     name: {
+//         type: Sequelize.STRING,
+//         allowNull: false
+//     }
+// }, {
+//     charset: 'utf8',
+//     collate: 'utf8_unicode_ci'
+//     });
 
 
-    var ProductType = sequelize.define('ProductType', {
-        productTypeId: {
-            type: Sequelize.UUID,
-            defaultValue: Sequelize.UUIDV1,
-            allowNull: false,
-            primaryKey: true
-        },
-        name: {
-            type: Sequelize.STRING,
-            allowNull: false
-        },
-        categoryId: {
-            type: Sequelize.UUID,
-            allowNull: false
-        }
-    }, {
-        charset: 'utf8',
-        collate: 'utf8_unicode_ci'
-    });
+//     var ProductType = sequelize.define('ProductType', {
+//         productTypeId: {
+//             type: Sequelize.UUID,
+//             defaultValue: Sequelize.UUIDV1,
+//             allowNull: false,
+//             primaryKey: true
+//         },
+//         name: {
+//             type: Sequelize.STRING,
+//             allowNull: false
+//         },
+//         categoryId: {
+//             type: Sequelize.UUID,
+//             allowNull: false
+//         }
+//     }, {
+//         charset: 'utf8',
+//         collate: 'utf8_unicode_ci'
+//     });
 
-Category.hasMany(ProductType, {
-  foreignKey: 'categoryId'
-});
+// Category.hasMany(ProductType, {
+//   foreignKey: 'categoryId'
+// });
 
-ProductType.belongsTo(Category, {
-  foreignKey: 'categoryId'
-});
+// ProductType.belongsTo(Category, {
+//   foreignKey: 'categoryId'
+// });
 
-Category.sync();
-ProductType.sync();
+// Category.sync();
+// ProductType.sync();
